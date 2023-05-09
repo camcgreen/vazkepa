aefddbe6532a726ea2370a643e8490735a09e672
FelixChirinosLopez <felixchirinoslopez@gmail.com>
2023-05-09 13:26:46 +0100

    screenSaver added

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -1,14 +1,21 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import '../styles/globals.css';
+import IdleTimerComponent from '../components/IdleTimer';
+
 
 export default function App({ Component, pageProps }) {
+
+
+
     return (
         <>
             <Head>
                 <title>BOILERPLATE</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
+            <IdleTimerComponent />
+
             <Component {...pageProps} />
         </>
     );
